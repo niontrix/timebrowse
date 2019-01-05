95227b00a356f851bb7af2d9cc155048e4bb5cf3
Tobias Heinlein <dev@tobias-heinlein.com>
2019-01-05 17:46:24 +0100

    Fix syntax issues with python3

--- nilfs2_ss_manager/nilfs2.py
+++ nilfs2_ss_manager/nilfs2.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
     prev = a.pop(0)
     for e in a:
         if e['date'] == prev['date']:
-            print "%d is same as %d" % (e['cno'], prev['cno'])
-            print e['date']
-            print prev['date']
+            print("%d is same as %d" % (e['cno'], prev['cno']))
+            print(e['date'])
+            print(prev['date'])
         else:
-            print "%d is different from %d" % (e['cno'], prev['cno'])
+            print("%d is different from %d" % (e['cno'], prev['cno']))
         prev = e
        
