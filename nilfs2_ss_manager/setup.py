df6e955da4181dfca97d4d49bd91f160d40af6d2
Tobias Heinlein <dev@tobias-heinlein.com>
2019-01-05 17:49:05 +0100

    Update setup.py
    Update setup.py with new version and author information

--- nilfs2_ss_manager/setup.py
+++ nilfs2_ss_manager/setup.py
@@ -3,9 +3,9 @@ import sys
 
 setup(
   name = 'nilfs2_ss_manager',
-  version = '0.6',
-  author = 'Jiro SEKIBA',
-  author_email = 'jir@unicus.jp',
+  version = '0.6.1',
+  author = 'Tobias Heinlein, Jiro SEKIBA',
+  author_email = 'dev@tobias-heinlein.com, jir@unicus.jp',
   py_modules = ['nilfs2'],
   scripts = ['nilfs2_ss_manager'],
   description = 'nilfs2 snapshot manager',
